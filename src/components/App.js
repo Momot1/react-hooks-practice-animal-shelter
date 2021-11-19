6ad09cfa4f87ba713073e634fc3320c3f0ab2e46
Mark Momot <markmomot03@gmail.com>
2021-11-19 16:31:34 -0500

    commit tree a8b01ece5f50a03bef37286f5813ac00218055ac

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,37 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function updateFilter(newFilter){
+    setFilters({type: `${newFilter}`})
+  }
+
+  function grabPets(){
+    let queryParameter = ""
+    if(filters.type === "cat"){
+      queryParameter = "?type=cat"
+    } else if(filters.type === "dog"){
+      queryParameter = "?type=dog"
+    } else if(filters.type === "micropig"){
+      queryParameter = "?type=micropig"
+    }
+
+    fetch(`http://localhost:3001/pets/${queryParameter}`)
+      .then(resp => resp.json())
+      .then(data => setPets(data))
+  }
+
+  function updatePet(id){
+    const updatedPetsAry = pets.map(pet => {
+      if(pet.id === id){
+        return {...pet, isAdopted:true}
+      } else{
+        return pet
+      }
+    })
+
+    setPets(updatedPetsAry)
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +46,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={updateFilter} onFindPetsClick={grabPets}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={updatePet}/>
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,8 +2,10 @@ import React from "react";
 
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({ pets, onAdoptPet }) {
+  const petsElements = pets.map(pet => <Pet key={pet.id} onAdoptPet = {onAdoptPet} pet={pet}/>)
+
+  return <div className="ui cards">{petsElements}</div>;
 }
 
 export default PetBrowser;
